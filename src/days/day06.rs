3dffbe5a05c947fddc23f041c12ca84d9bf2e10e
GodGotzi <eli.gottsbacher@gmail.com>
2023-12-06 11:29:38 +0100

    day05

---
+++ src/days/day06.rs
@@ -0,0 +1,91 @@
+use crate::api::Solution;
+
+pub struct Day06;
+
+impl Solution for Day06 {
+    fn get_day(&self) -> u8 {
+        6
+    }
+
+    fn part1(&self, input: String) -> String {
+        let lines = input.lines().collect::<Vec<&str>>();
+
+        let time = lines[0]
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split(' ')
+            .filter(|s| !s.starts_with(' ') && !s.is_empty())
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+
+        let distance = lines[1]
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split(' ')
+            .filter(|s| !s.starts_with(' ') && !s.is_empty())
+            .map(|s| s.trim().parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+
+        let mut product = 1;
+
+        for index in 0..time.len() {
+            let time = time[index];
+            let distance = distance[index];
+            let mut sum = 0;
+
+            for i in 1..time {
+                let new_distance = i * (time - i);
+
+                if new_distance > distance {
+                    sum += 1;
+                }
+            }
+
+            product *= sum;
+        }
+
+        product.to_string()
+    }
+
+    fn part2(&self, input: String) -> String {
+        let lines = input.lines().collect::<Vec<&str>>();
+
+        let time = lines[0]
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split(' ')
+            .filter(|s| !s.starts_with(' ') && !s.is_empty())
+            .collect::<String>()
+            .trim()
+            .parse::<u128>()
+            .unwrap();
+
+        let distance = lines[1]
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split(' ')
+            .filter(|s| !s.starts_with(' ') && !s.is_empty())
+            .collect::<String>()
+            .trim()
+            .parse::<u128>()
+            .unwrap();
+
+        println!("{} {}", time, distance);
+
+        let mut sum: u128 = 0;
+
+        for i in 1..time {
+            let new_distance = i * (time - i);
+
+            if new_distance > distance {
+                sum += 1;
+            }
+        }
+
+        sum.to_string()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod days;
 use api::Solution;
 
 fn main() {
-    let solutions: Vec<Box<dyn Solution>> = vec![Box::new(days::day01::Day01)];
+    let solutions: Vec<Box<dyn Solution>> = vec![Box::new(days::day06::Day06)];
 
     for solution in solutions {
         run_solution(&*solution);
